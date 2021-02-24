b5d3d9ed31a1c39eda63ef5232117f99c1da24d2
Ubuntu <ubuntu@ip-172-31-0-78.ap-southeast-1.compute.internal>
2021-02-24 07:33:51 +0000

    24 feb 2021

--- client_script/main.py
+++ client_script/main.py
@@ -11,22 +11,22 @@ parser.parse_args()
 args = parser.parse_args()
 
 if args.input and args.output:
-    with open(str(args.input), newline = '', encoding='utf-8') as csvfile:
-        reader = csv.DictReader(csvfile)
+    with open(str(args.input), newline = '', encoding='utf-8', errors='ignore') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=";")
         with open(str(args.output), 'a', newline='') as file:
-            fieldnames = ['Name', 'transactionHash', 'status', 'to', 'blockNumber']
+            fieldnames = ['Name', 'Email', 'Institute', 'Grade', 'transactionHash', 'status', 'to', 'blockNumber']
             writer = csv.DictWriter(file, fieldnames=fieldnames)
             writer.writeheader()
             for row in reader:
                 print(row['Name'])
                 process = subprocess.run(
-                    ['node', 'script.js', row['Name'], row['University']],
+                    ['node', 'script.js', row['Name'], row['Institute']],
                     text=True,
                     stdout=subprocess.PIPE,
                     check=True
                 )
                 for line in process.stdout.split():
-                    nameConverted = { "Name": row['Name'] }
+                    nameConverted = { "Name": row['Name'], "Email": row["Email"], "Institute": row["Institute"], "Grade": row["Grade"] }
                     data = json.loads(line)
                     data.update(nameConverted)
                     writer.writerow(data)
